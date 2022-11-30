fd6d930c791db0103e92954f0e18a92d1fb6a9c6
Karol <kariurek@gmail.com>
2022-11-30 10:12:47 +0100

    Feat: add header and left menu base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,85 @@
-import React from 'react';
-import routes from '@/routes/index';
-import { useRoutes } from 'react-router-dom';
+import { Button } from '@chakra-ui/react'
+
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Spacer,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
+
+import { HamburgerIcon, SettingsIcon } from '@chakra-ui/icons'
+
+import { useState } from 'react'
 
 const App = () => {
-  const element = useRoutes(routes);
+  const [drawerOpen, setDrawerOpen] = useState(false)
 
-  return <>{element}</>;
-};
+  const optionsBeforeRepair = ['Holowanie z miejsca awarii', 'Auto zastępcze', 'Dojazd do miejsca awarii']
 
-export default App;
+  const optionsAfterRepair = ['Bonus za naprawę na miejscu', 'Odwiezienie < 50 km']
+
+  const appHeader = (
+    <Flex align="center" padding={'16px'} gap="">
+      <Button onClick={() => setDrawerOpen(true)}>
+        <HamburgerIcon />
+      </Button>
+      <Spacer />
+      <Box>Dane finansowe apki</Box>
+      <Spacer />
+      <Button>
+        <SettingsIcon />
+      </Button>
+    </Flex>
+  )
+
+  const servicesDrawer = (
+    <Drawer isOpen={drawerOpen} placement="left" onClose={() => setDrawerOpen(false)}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>Wybierz usługę mobilności</DrawerHeader>
+
+        <DrawerBody>
+          <Stack align={'center'}>
+            <Text>Usługi przed naprawą</Text>
+            {optionsBeforeRepair.map((option) => (
+              <Button width="100%" key={option} placeholder={option}>
+                {option}
+              </Button>
+            ))}
+            <Text>Usługi po naprawie</Text>
+            {optionsAfterRepair.map((option) => (
+              <Button width="100%" key={option} placeholder={option}>
+                {option}
+              </Button>
+            ))}
+          </Stack>
+        </DrawerBody>
+
+        <DrawerFooter>
+          <Button variant="outline" mr={3} onClick={() => setDrawerOpen(false)}>
+            Cancel
+          </Button>
+          <Button colorScheme="blue">Save</Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+
+  return (
+    <>
+      {appHeader}
+      {servicesDrawer}
+    </>
+  )
+}
+
+export default App
