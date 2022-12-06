3dd5b6d2d2a2942410d5e3ba537f65c5784e7d63
Karol <kariurek@gmail.com>
2022-12-06 08:24:52 +0100

    WIP: add placeholder components

--- src/components/pickedServiceList/pickedServiceList.tsx
+++ src/components/pickedServiceList/pickedServiceList.tsx
@@ -1,7 +1,9 @@
-import { Stack } from '@chakra-ui/react'
+import { HStack, Input, Select, Stack } from '@chakra-ui/react'
+import { rentCars } from '../../store/servicesStore/rentCars'
 import { Service } from '../../store/servicesStore/servicesTypes'
 import { DocumentationBonus } from '../services/documentationBonus'
 import { Towing } from '../services/towing'
+import { ServiceListItem } from './serviceListItem/serviceListItem'
 
 type Props = {
   services: Service[]
@@ -11,6 +13,76 @@ export const PickedServiceList = ({ services }: Props) => {
     <Stack overflowY="auto" shadow="dark-lg" rounded="xl" flex="1" h="100%" bg="chakra-subtle-bg">
       <DocumentationBonus />
       <Towing />
+      <ServiceListItem
+        service={{
+          active: true,
+          badges: ['przed naprawą'],
+          price: 100,
+          title: 'Auto zastępcze',
+          preventCombineGroup: null,
+          serviceType: 'before repair',
+        }}
+      >
+        <Select flex={'0 0 400px'} placeholder="Wybierz model">
+          {rentCars.map((car) => (
+            <option key={car.name} value={car.name}>
+              {car.name}
+            </option>
+          ))}
+        </Select>
+      </ServiceListItem>
+      <ServiceListItem
+        service={{
+          active: true,
+          badges: ['przed naprawą'],
+          price: 100,
+          title: 'Dojazd do miejsca awarii',
+          preventCombineGroup: null,
+          serviceType: 'before repair',
+        }}
+      >
+        <HStack>
+          <Input placeholder="RBH" type="number" />
+          <Input placeholder="stawka" type="number" />
+        </HStack>
+      </ServiceListItem>
+      <ServiceListItem
+        service={{
+          active: true,
+          badges: ['po naprawie', 'ryczałt'],
+          price: 100,
+          title: 'Bonus za naprawę na miejscu',
+          preventCombineGroup: null,
+          serviceType: 'after repair',
+        }}
+        textToCopy="Bonus 100 zł za naprawę na miejscu"
+      />
+      <ServiceListItem
+        service={{
+          active: true,
+          badges: ['po naprawie', 'ryczałt'],
+          price: 100,
+          title: 'Odwiezienie pojazdu',
+          preventCombineGroup: null,
+          serviceType: 'after repair',
+        }}
+        textToCopy="Odwiezienie pojazdu klienta do 50 km 300 zł netto
+"
+      />
+      <ServiceListItem
+        service={{
+          active: true,
+          badges: ['po naprawie'],
+          price: 100,
+          title: 'Odwiezienie pojazdu',
+          preventCombineGroup: null,
+          serviceType: 'after repair',
+        }}
+        textToCopy={`OPŁATA MANIPULACYJNA = 60,03 EURO NETTO  x KURS 0 = 0,00 zł
+OPŁATA ZA ODLEGŁOŚĆ 1,41 EUR/km NETTO X 51 km x KURS 0 = 0,00 zł NETTO
+RAZEM = 0,00 ZL NETTO
+`}
+      />
     </Stack>
   )
 }
---
+++ src/store/servicesStore/rentCars.ts
@@ -0,0 +1,23 @@
+type Car = {
+  name: string
+  price: number
+  id: string
+}
+
+export const rentCars: Car[] = [
+  {
+    name: 'Audi A4',
+    price: 100,
+    id: '1',
+  },
+  {
+    name: 'Audi A6',
+    price: 200,
+    id: '2',
+  },
+  {
+    name: 'Audi A8',
+    price: 300,
+    id: '3',
+  },
+]
