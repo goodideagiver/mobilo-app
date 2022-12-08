63b85d2c838ea2dc02c008b3bcd56aec532511de
Karol <kariurek@gmail.com>
2022-12-08 13:18:32 +0100

    Feat: add information in picked service list if there is no services picked

--- src/components/pickedServiceList/pickedServiceList.tsx
+++ src/components/pickedServiceList/pickedServiceList.tsx
@@ -1,4 +1,5 @@
-import { Stack } from '@chakra-ui/react'
+import { Center, Stack, Text } from '@chakra-ui/react'
+import { useServicesStore } from '../../store/servicesStore/servicesStore'
 import { DocumentationBonus } from '../services/documentationBonus'
 import { DriveToClient } from '../services/driveToClient'
 import { RentCar } from '../services/rentCar'
@@ -7,14 +8,42 @@ import { TowBack } from '../services/towBack'
 import { Towing } from '../services/towing'
 
 export const PickedServiceList = () => {
-  return (
-    <Stack overflowY="auto" shadow="dark-lg" rounded="xl" flex="1" h="100%" bg="chakra-subtle-bg">
+  const hasServicesToDisplay =
+    useServicesStore((state) => state.services).filter((service) => service.active).length > 0
+
+  const services = (
+    <>
       <DocumentationBonus />
       <Towing serviceId="1" />
       <RentCar />
       <DriveToClient />
       <RepairOnSiteBonus />
       <TowBack />
+    </>
+  )
+
+  const noPickedServiceMessage = (
+    <Center h="100%">
+      <Text fontSize="2xl" fontWeight="bold" fontStyle="italic">
+        Nie wybrano jeszcze żadnej usługi
+      </Text>
+    </Center>
+  )
+
+  return (
+    <Stack
+      opacity={hasServicesToDisplay ? '1' : '0.5'}
+      border="2px"
+      borderColor="gray.600"
+      overflowY="auto"
+      shadow="dark-lg"
+      rounded="xl"
+      transition="opacity 0.3s ease-in-out"
+      flex="1"
+      h="100%"
+      bg="chakra-subtle-bg"
+    >
+      {hasServicesToDisplay ? services : noPickedServiceMessage}
     </Stack>
   )
 }
