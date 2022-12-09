a8087601edcc4097e3495455dfa55712c516b0ce
Karol <kariurek@gmail.com>
2022-12-09 12:35:35 +0100

    Feat: add rent car logic

--- src/components/services/rentCar.tsx
+++ src/components/services/rentCar.tsx
@@ -1,16 +1,49 @@
 import { FormControl, FormLabel, Input, Select } from '@chakra-ui/react'
+import { ChangeEventHandler } from 'react'
 import { rentCars } from '../../store/servicesStore/rentCars'
 import { useServicesStore } from '../../store/servicesStore/servicesStore'
 import { ServiceListItem } from '../pickedServiceList/serviceListItem/serviceListItem'
 
+import { useState } from 'react'
+import { numberToOutputCurrencyString } from '../../helpers/numberToOutputCurrencyString'
+
 export const RentCar = () => {
+  const [days, setDays] = useState(0)
+  const [carPrice, setCarPrice] = useState(0)
+  const [carName, setCarName] = useState('')
+
   const rentCarServiceStore = useServicesStore((state) => state.services.find((service) => service.id === '2'))
+
+  const setPrice = useServicesStore((state) => state.setServicePrice)
 
   const isActive = rentCarServiceStore?.active
 
   if (!isActive) return null
 
-  const daysInputHandler = () => {}
+  const daysInputHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const days = event.target.valueAsNumber
+    setDays(days)
+
+    if (isFinite(days) && isFinite(carPrice)) {
+      setPrice('2', days * carPrice)
+    }
+  }
+
+  const carSelectHandler: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    const carPrice = rentCars.find((car) => car.name === event.target.value)?.price
+
+    setCarName(event.target.value)
+
+    if (carPrice) {
+      setCarPrice(carPrice)
+    }
+
+    if (isFinite(days) && carPrice && isFinite(carPrice)) {
+      setPrice('2', days * carPrice)
+    }
+  }
+
+  const shouldDisplayText = days > 0 && carName !== '' && carPrice > 0
 
   return (
     <ServiceListItem
@@ -18,17 +51,23 @@ export const RentCar = () => {
       service={{
         active: true,
         badges: ['przed naprawą'],
-        price: 100,
+        price: rentCarServiceStore.price,
         title: 'Auto zastępcze',
         preventCombineGroup: null,
         serviceType: 'before repair',
       }}
-      textToCopy={`KLASA A AUTO ZASTĘPCZE NA 12 DNI X STAWKA 240 zł netto za dobę = 2880 zł
-`}
+      textToCopy={
+        shouldDisplayText
+          ? `AUTO ZASTĘPCZE: ${carName} NA ${days} DNI X STAWKA ${numberToOutputCurrencyString(
+              carPrice,
+            )} netto za dobę = ${numberToOutputCurrencyString(rentCarServiceStore.price)}
+`
+          : 'Nie wybrano modelu auta lub dni'
+      }
     >
       <FormControl flex="0 0 200px">
         <FormLabel>Model</FormLabel>
-        <Select flex={'0 0 200px'} placeholder="Wybierz model">
+        <Select flex={'0 0 200px'} onChange={carSelectHandler} placeholder="Wybierz model">
           {rentCars.map((car) => (
             <option key={car.name} value={car.name}>
               {car.name}
@@ -38,7 +77,7 @@ export const RentCar = () => {
       </FormControl>
       <FormControl flex="0 0 200px">
         <FormLabel>Dni</FormLabel>
-        <Input onChange={daysInputHandler} value={0} min="0" placeholder="days" type="number" />
+        <Input step={0} onChange={daysInputHandler} value={days} min="0" placeholder="days" type="number" />
       </FormControl>
     </ServiceListItem>
   )
